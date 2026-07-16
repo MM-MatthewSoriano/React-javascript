c8c104011702df7d20d22b5ab8eaf950d518fa58
MM-MatthewSoriano <matthewsoriano763@gmail.com>
2026-07-16 17:26:10 +0800

    chore: lesson3 added

---
+++ lesson3.js
@@ -0,0 +1,58 @@
+// exc 1
+// check if age is minor or adult
+
+// const age = 20;
+// if (age >= 18) {
+//     console.log("You are an adult");
+// } else {
+//     console.log("You are a minor");
+// }
+
+// exc 2
+// check if the temperature is hot or not
+
+const temperature = 32;
+if (temperature >= 30) {
+    console.log("It's hot today.");
+} else {
+    console.log("It's not hot today.");
+}
+
+// exc 3
+// grade the score
+const score = 78;
+if (score >= 90) {
+    console.log("Excellent");
+} else if (score >= 75 && score < 90) {
+    console.log("Good");
+} else if (score >= 50 && score < 75) {
+    console.log("Pass");
+} else {
+    console.log("Fail")
+}
+
+// exc 4
+// Check if the user is at least 18 and has a ticket
+const age = 25;
+const hasTicket = true;
+
+if (age >= 18 && hasTicket) {
+    console.log("You may enter.");
+} else {
+    console.log("Entry denied.")
+}
+
+// exc 5
+const username = "Matt";
+const password = "123456";
+
+if (username == "Matt" && password == "123456") {
+    console.log("Login successful.")
+} else {
+    console.log("Invalid username and password.")
+}
+
+// challenge question answer
+// can watch movie
+
+
