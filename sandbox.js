3dc1c3fd52ef21071a9de6d01ce404e6fdc9b6b3
MM-MatthewSoriano <matthewsoriano763@gmail.com>
2026-07-16 20:47:33 +0800

    chore: random sandbox daskdjslad

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,42 @@
 // resolving arrow functions
-const person = {
-    name: "Matt",
-
-    sayHello: function() {
-        console.log("Hello!");
+const person1 = {
+    nigga: "Matt",
+    sayHello () {
+        console.log(this.nigga);
+    },
+    sayBye: function () {
+        console.log(this.nigga);
+    },
+    sayNothing: () => {
+        console.log(person1.nigga);
     }
 };
 
+person1.sayHello();
+person1.sayBye();
+person1.sayNothing();
+
+// IIFE
+(function() {
+    console.log("Hello");
+})();
+
+// CAllback
+setTimeout(function () {
+    console.log("Inside callback");
+},2000);
+
+// problem with regular functions in callbacks
+const out = {
+    name: "Matt",
+    person: {
+        sayHello: function() {
+
+            setTimeout(() => {
+                console.log(this.name);
+            }, 3000);
+
+        }
+    }
+}
 person.sayHello();
