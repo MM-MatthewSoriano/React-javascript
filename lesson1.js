3f40e0ed7eba30dc270c1a37a094c5c8b35f9b33
MM-MatthewSoriano <matthewsoriano763@gmail.com>
2026-07-16 17:03:38 +0800

    chore: uploading lessons 1-2 javascript to react learning

---
+++ lesson1.js
@@ -0,0 +1,36 @@
+// exc 1
+// create variables for your name, age, employment status
+
+let name = "Matt"
+let age = 22
+let employmentStatus = "Unemployed"
+
+console.log ("1.", name, age, employmentStatus)
+
+// exc 2
+// create constants for country, favorite language
+
+const country = "Philippines"
+const favoriteLanguage = "Japanese"
+
+console.log("2.",country,favoriteLanguage)
+
+// exc3
+// create variables for name, age, city
+
+let newName = "Matt"
+let newAge = 21
+let city = "Pasay"
+
+console.log("3.")
+console.log(`My name is ${newName}.`);
+console.log(`I am ${newAge} years old.`);
+console.log(`I live in ${city}.`);
+
+// output
+// 1. Matt 22 Unemployed
+// 2. Philippines Japanese
+// 3.
+// My name is Matt.
+// I am 21 years old.
+// I live in Pasay.
---
+++ lesson2.js
@@ -0,0 +1,44 @@
+// exc 1
+// create 2 variables and perform arithmetic operations, print the result
+const num1 = 25;
+const num2 = 5;
+
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1%num2);
+console.log(num1**num2);
+
+// exc 2
+// perform arithmetic operations on the score using assignment operators
+let score = 50;
+
+score += 10;
+score *= 2;
+score -= 15;
+
+console.log(score);
+
+// exc 3
+// Compare the variables and print the result
+
+const age = 18;
+console.log(age >= 18);
+const username = "Matt";
+console.log(username === "Matt");
+
+// exc 4
+// Predict the output before running the code
+// 20
+console.log(10 + 5 * 2);
+// 30
+console.log((10 + 5) * 2);
+// True
+console.log("5" == 5);
+// False
+console.log("5" === 5);
+// 2
+console.log(20 % 6);
+// 27
+console.log(3 ** 3);
