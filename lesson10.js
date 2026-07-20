b4c716bc981831f7c402e211a8281e1592d5c321
MM-MatthewSoriano <matthewsoriano763@gmail.com>
2026-07-20 11:07:52 +0800

    chore: lesson10

---
+++ lesson10.js
@@ -0,0 +1,74 @@
+// exc 1
+// A
+// C
+// B
+
+// console.log("A");
+
+// setTimeout(() => {
+//     console.log("B");
+// }, 1000);
+
+// console.log("C");
+
+// exc 2
+// create a promise that immediately resolves with Learnig javascript
+
+// const promise = new Promise((resolve,reject) => {
+//     resolve("Learning Javascript!");
+// });
+
+// promise.then(result => {
+//     console.log(result);
+// });
+
+// exc 3
+// create a promise that immediately rejects with something went wrong
+
+const promise = new Promise((resolve,reject) => {
+    reject("Something went wrong!");
+});
+
+promise.catch(error => {
+    console.log(error);
+});
+
+// exc 4
+// write an async function that prints "Hello Async"
+async function sayHello() {
+    return "Hello Async";
+}
+
+sayHello().then(result=>{
+    console.log(result);
+});
+
+// exc 5
+// without changing the function explain:
+// Why .then() works here.
+// What the output is.
+// .then() retrieves the result from the promise that was the output of the async function
+// 20
+
+async function getNumber() {
+    return 10;
+}
+
+getNumber().then(number => {
+    console.log(number * 2);
+});
+
+// challenge question
+// 
+console.log("Start");
+
+setTimeout(() => {
+    console.log("Middle");
+}, 0);
+
+console.log("End");
+
+
+
+
+
--- sandbox.js
+++ sandbox.js
@@ -1,42 +1,78 @@
-// resolving arrow functions
-const person1 = {
-    nigga: "Matt",
-    sayHello () {
-        console.log(this.nigga);
-    },
-    sayBye: function () {
-        console.log(this.nigga);
-    },
-    sayNothing: () => {
-        console.log(person1.nigga);
-    }
-};
+// // resolving arrow functions
+// const person1 = {
+//     nigga: "Matt",
+//     sayHello () {
+//         console.log(this.nigga);
+//     },
+//     sayBye: function () {
+//         console.log(this.nigga);
+//     },
+//     sayNothing: () => {
+//         console.log(person1.nigga);
+//     }
+// };
 
-person1.sayHello();
-person1.sayBye();
-person1.sayNothing();
+// person1.sayHello();
+// person1.sayBye();
+// person1.sayNothing();
 
-// IIFE
-(function() {
-    console.log("Hello");
-})();
+// // IIFE
+// (function() {
+//     console.log("Hello");
+// })();
 
-// CAllback
-setTimeout(function () {
-    console.log("Inside callback");
-},2000);
+// // CAllback
+// setTimeout(function () {
+//     console.log("Inside callback");
+// },2000);
 
-// problem with regular functions in callbacks
-const out = {
-    name: "Matt",
-    person: {
-        sayHello: function() {
+// // problem with regular functions in callbacks
+// const out = {
+//     name: "Matt",
+//     person: {
+//         sayHello: function() {
 
-            setTimeout(() => {
-                console.log(this.name);
-            }, 3000);
+//             setTimeout(() => {
+//                 console.log(this.name);
+//             }, 3000);
 
-        }
-    }
+//         }
+//     }
+// }
+
+// callback
+function greet(callback) {
+    console.log("Hey");
+    callback();
 }
-person.sayHello();
+
+greet(() => {
+    console.log("Bye");
+});
+
+// // promises
+// const promise = new Promise((resolve, reject) => {
+
+//     resolve("Success");
+
+// });
+
+// promise.then(result => {
+//     console.log(result);
+// });
+
+const promise = new Promise((resolve, reject) => {
+
+    reject("Error");
+
+});
+
+promise
+    .then(result => {
+        console.log(result);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+
